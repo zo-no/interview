918ed61cfd6f328e5fc74e1d42e0a3537a230697
zo-no <2742160682@qq.com>
2024-02-15 09:06:52 +0800

    同步

---
+++ 0/1.js
@@ -0,0 +1,16 @@
+/**
+ * @Date        2024/02/13 22:21:55
+ * @Author      zono
+ * @Description 100
+ * */
+
+/**
+* 1.
+* @param {type}  
+* @returns   
+* */
+function a() {
+  while (sum = 100) {
+      
+    }
+}
---
+++ 手写题/防抖.js
@@ -0,0 +1,11 @@
+/**
+ * @Date        2024/02/14 14:43:18
+ * @Author      zono
+ * @Description 防抖练习
+ * */
+
+/**
+* 第一次学习
+* @param {type}  
+* @returns   
+* */
